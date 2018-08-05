53c2b42ff7b81d108e419fc2f5ff540647633541
Albert Yanit <ayanit1@googlemail.com>
2018-08-05 21:28:27 +0100

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from '../../components/App/style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        <h1>PIPELINE DASHBOARD</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
